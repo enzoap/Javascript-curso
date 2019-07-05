9c9078a9dade7c133cd61a86e10eae5ee78302de
enzoap <enzoap2009@hotmail.com>
2019-07-05 17:11:31 -0300

    Comentarios

---
+++ guanabara/aula 013/ambiente.js
@@ -0,0 +1,25 @@
+var i = 1
+do {
+    console.log(`Passo ${i}`)
+    i++
+} while (i <= 3)
+
+
+
+
+/* var i = 1
+while (i <= 3){
+    console.log(`Passo ${i}`)
+    i++
+} */
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
