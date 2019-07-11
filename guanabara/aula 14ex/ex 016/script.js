3413dc7fd15cfcbe5d8eb043d3014fd74650ada1
enzoap <enzoap2009@hotmail.com>
2019-07-11 17:57:56 -0300

    Refiz o exercíco

--- guanabara/aula 14ex/ex 016/script.js
+++ guanabara/aula 14ex/ex 016/script.js
@@ -1,34 +1,33 @@
-var inicio = document.querySelector('#inicio')
-var fim = document.querySelector('#fim')
-var passo = document.querySelector('#passo')
-var res = document.querySelector('#res')
-
-
-function contar(){
-    if (inicio.value.length ==0 || fim.value.length == 0 || passo.value.length ==0){
-        res.innerHTML="Impossível contar!"  
-        //alert('[ERRO] dados incompletos')       
+function contar() {
+    var inicio = document.querySelector('#inicio')
+    var fim = document.querySelector('#fim')
+    var passo = document.querySelector('#passo')
+    var res = document.querySelector('#res')
+    if (inicio.value.length == 0){
+        res.innerHTML = 'Erro, verifique os dados <br>'
+    } else if (fim.value.length == 0) {
+        res.innerHTML = 'Erro, verifique os dados'
     }else {
-        res.innerHTML = 'Contando: <br>'
-        var inicioValue = Number(inicio.value)
-        var fimValue = Number(fim.value)
-        var passoValue = Number(passo.value)
-        if (passoValue <= 0){
-            alert('Passo invalido! Considerando PASSO 1')
-            passoValue = 1
+        res.innerHTML = 'Contando <br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0){
+            alert('Erro passo igual a 0, definindo passo igual a 1')
+            p = 1;
         }
-        if (inicioValue < fimValue){
+        if (i < f){
             //Contagem crescente
-            for (var c = inicioValue;c <=fimValue; c+=passoValue){
-                res.innerHTML += ` ${c} \u{1f449}`
-            }
-        }else {
-            //Contagem regressiva
-            for (var c = inicioValue;c >=fimValue;c-=passoValue){
-                res.innerHTML += ` ${c} \u{1f449}`
-            }
+            for (var j = i;j<=f;j+=p){
+                res.innerHTML += ` ${j} \u{1f449} `
+            }   
         }
-        res.innerHTML += `\u{1f3c1}`
+        if ( i > f){
+            //Contagem regressiva
+            for (var j = i;j>=f;j-=p){
+                res.innerHTML += ` ${j} \u{1f449}`
+            }   
+        }
+        res.innerHTML += `\u{1f3c1}` 
     }
-        
 }
