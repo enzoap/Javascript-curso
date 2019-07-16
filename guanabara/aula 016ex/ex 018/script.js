26aba38bc51d7b3ed28b9a8163a91c1468cbb410
enzoap <enzoap2009@hotmail.com>
2019-07-15 23:02:06 -0300

    ex 018

---
+++ guanabara/aula 016ex/ex 018/script.js
@@ -0,0 +1,42 @@
+var inputElement = document.querySelector('#num')
+var divres = document.querySelector('#res')
+var select = document.querySelector('#tab')
+var number = []
+
+
+
+function adicionar(){
+    if (inputElement.value <= 0 || inputElement.value >=101 || inputElement.value.length == 0){
+        alert('Número invalido ou já esta na lista')
+    }else  {
+        divres.innerHTML = ''
+        var element = Number(inputElement.value)
+        var idx = number.indexOf(element)
+        if (idx === -1){
+            //var numero = inputElement.value
+            var colocarNumero = number.push(element)
+            var apresentarNumero = number.slice(-1)[0]
+            var optionElement = document.createElement('option')
+            optionElement.innerHTML = `${apresentarNumero}`
+            select.appendChild(optionElement)
+        }else {
+            alert('Número invalido ou já esta na lista')
+        }
+        
+    }
+}
+
+
+
+ function finalizar (){
+    var reducer = (acumulator,currentValue) => acumulator + currentValue;
+    var total = number.reduce(reducer)
+    var media = total / number.length
+
+    divres.innerHTML = `Ao todo, temos ${number.length} números cadastrados.<br>`
+    divres.innerHTML += `O maior número informado foi ${Math.max.apply(null, number)}.<br>`
+    divres.innerHTML += `O menor número informado foi ${Math.min.apply(null, number)}.<br>`
+    divres.innerHTML += `A soma dos valores é ${total}.<br>`
+    divres.innerHTML += `A média dos valores é ${media}`
+    
+} 
